db70b9094aea11b48f4901e4e4e84c7c13625679
Jason Walton <jwalton@solinkcorp.com>
2020-09-02 10:14:48 -0400

    chore: Improve jest test times.

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     transform: {
         '.(ts|tsx)': 'ts-jest',
     },
+    testEnvironment: 'node',
     testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
     moduleFileExtensions: ['ts', 'tsx', 'js'],
 };
